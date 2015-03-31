60a2e1aaaad97472f905bc5313f5c52b56781dee
Nate Collings <nate@natecollings.com>
2015-03-31 17:38:26 -0600

    basic lib file

---
+++ lib/acts_as_redditable.rb
@@ -0,0 +1,15 @@
+require 'active_record'
+
+module Acts
+  module Redditable
+    extend ActiveSupport::Concern
+
+    module ClassMethods
+      def acts_as_redditable
+        has_one :reddit_post
+      end
+    end
+  end
+end
+
+ActiveRecord::Base.send(:include, Acts::Redditable)
