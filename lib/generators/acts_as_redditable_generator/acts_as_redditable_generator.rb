f921dbe5846d972ea66f13afae0f227c9e58e31f
Nate Collings <nate@natecollings.com>
2015-03-31 18:37:26 -0600

    start generator

---
+++ lib/generators/acts_as_redditable_generator/acts_as_redditable_generator.rb
@@ -0,0 +1,18 @@
+class ActsAsCommentableWithThreadingMigrationGenerator < Rails::Generators::Base
+  include Rails::Generators::Migration
+  
+  source_root File.expand_path('../templates', __FILE__)
+  
+  def self.next_migration_number(dirname)
+    if ActiveRecord::Base.timestamped_migrations
+      Time.now.utc.strftime("%Y%m%d%H%M%S")
+    else
+      "%.3d" % (current_migration_number(dirname) + 1)
+    end
+  end
+
+  def manifest
+    migration_template 'migration.rb', 'db/migrate/acts_as_redditable_migration.rb'
+    copy_file 'reddit_post.rb', 'app/models/reddit_post.rb'
+  end
+end
