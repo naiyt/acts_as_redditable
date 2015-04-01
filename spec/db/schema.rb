bc7009665b0da11986984b32a165119c16e63cbf
Nate Collings <nate@natecollings.com>
2015-03-31 19:13:24 -0600

    schema fixture

---
+++ spec/db/schema.rb
@@ -0,0 +1,17 @@
+ActiveRecord::Schema.define(:version => 1) do
+  create_table "builds", :force => true do |t|
+    t.timestamps
+  end
+
+  create_table "reddit_posts", :force => true do |t|
+    t.integer :redditable_id
+    t.string :redditable_type
+    t.string :url
+    t.string :reddit_owner
+    t.integer :user_id, :null => false
+    t.timestamps
+  end
+
+  add_index :reddit_posts, :user_id
+  add_index :reddit_posts, [:redditable_id, :redditable_type]
+end
