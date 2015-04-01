2ad62c488c4b85729ceb893475a8bcf75bc57216
Nate Collings <nate@natecollings.com>
2015-03-31 18:45:46 -0600

    migration

---
+++ lib/generators/acts_as_redditable_generator/templates/migration.rb
@@ -0,0 +1,19 @@
+class ActsAsRedditableMigration < ActiveRecord::Migration
+  def self.up
+    create_table :reddit_posts, :force => true do |t|
+      t.integer :redditable_id
+      t.string :redditable_type
+      t.string :url
+      t.string :reddit_owner
+      t.integer :user_id, :null => false
+      t.timestamps
+    end
+
+    add_index :reddit_posts, :user_id
+    add_index :reddit_posts, [:redditable_id, :redditable_type]
+  end
+
+  def self.down
+    drop_table :reddit_posts
+  end
+end
